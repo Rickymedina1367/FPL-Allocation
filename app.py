1bc3f19e252745a9192b06bb7759dae30e9489c3
Rickymedina1367 <ricardo1367medina@gmail.com>
2025-04-09 09:06:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("FPL Allocation Tool")
 st.markdown("""
     <style>
     html, body, div, input, label {
-        font-size: 20px !important;
+        font-size: 25px !important;
     }
     </style>
 """, unsafe_allow_html=True)
